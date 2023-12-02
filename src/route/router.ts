a524e828b64a1ce493c9f3039e44c6a4b4abed2d
v1ctor <victorviniciuspj@gmail.com>
2023-12-02 16:56:09 -0300

    feat(User): add getUser and createUser

--- src/route/router.ts
+++ src/route/router.ts
@@ -2,8 +2,11 @@ import { Router } from "express";
 import { healthController } from "../controller/HealthController";
 import { chatRoomController } from "../controller/ChatRoomController";
 import { diceController } from "../controller/DiceController";
+import UserController from "../controller/UserController";
 
 const router: Router = Router();
+
+const _userController = UserController.getInstance();
 
 router.get("/chats", chatRoomController.getChats);
 router.post("/chats", chatRoomController.create);
@@ -12,6 +15,8 @@ router.post("/chats/connect", chatRoomController.connect); //TODO: put chatId in
 // router.get("/chats/messages", chatRoomController.getMessages);
 router.post("/chats/messages/:chatId/:userId", chatRoomController.post);
 
+router.get("/users", _userController.getUsers);
+router.post("/users", _userController.createUser);
 
 // router.get("/dices/:expression", diceController.rollDices);
 router.get("/dices/2d6", diceController.roll2d6);
