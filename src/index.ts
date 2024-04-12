2bc5840f3d42bb8755837f8d9e78f77299a70f3e
victor santos <victor.santos@tce.ma.gov.br>
2024-04-12 20:43:53 -0300

    fix/refact(index.ts): dotenv, init()

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { App } from "./app";
+import { config } from "dotenv";
+import { resolve } from "path";
+
+config({ path: resolve(__dirname, "../.env") });
 
 const port = process.env.PORT;
 
-const app = new App().server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+function init() {
+  return new App().server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+const app = init();
