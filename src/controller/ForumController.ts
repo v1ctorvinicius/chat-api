498eea9aaa7f8a2494cb31a823a3252eb3cee23a
victor.santos <victor.santos@gmail.com>
2024-06-20 09:35:24 -0300

    refactor

--- src/controller/ForumController.ts
+++ src/controller/ForumController.ts
@@ -1,34 +1,38 @@
 import { Request, Response } from "express";
 import validator from "validator";
-import { GetForumByIdUseCase } from "../ForumUseCases";
+import { CreateForumUseCase, GetForumByIdUseCase } from "../ForumUseCases";
 import { ForumRepository } from "../ForumRepository";
 import { ForumRepositoryRedisImpl, createRedisClient } from "../db/redis";
 
 const redisClient = createRedisClient();
-const forumRepository: ForumRepository = new ForumRepositoryRedisImpl(redisClient);
+const forumRepository: ForumRepository = new ForumRepositoryRedisImpl(
+  redisClient
+);
 const getForumByIdUseCase = new GetForumByIdUseCase(forumRepository);
+const createForumUseCase = new CreateForumUseCase(forumRepository);
 
 const getForums = async (req: Request, res: Response) => {
   return res.json(JSON.parse("ok")).status(200);
 };
 
 const createForum = async (req: Request, res: Response) => {
-  if (!req.body.userId || !req.body.name) return res.sendStatus(400);
+  if (
+    !req.body.name ||
+    !req.body.creatorId ||
+    !req.body.description ||
+    !validator.isUUID(req.body.creatorId)
+  ) {
+    return res.sendStatus(400);
+  }
+  const newForum = {
+    id: req.body.id,
+    name: req.body.name,
+    creatorId: req.body.creatorId,
+    description: req.body.description,
+  };
 
-  // const forumId = Date.now().toString();
-  // const response = await db.sadd(
-  //   `forum:${forumId}`,
-  //   JSON.stringify([
-  //     { userId: "req.body.userId", name: req.body.forumName },
-  //     { userId: req.body.userId, name: req.body.forumName },
-  //   ])
-  // );
-
-  // if (response !== "OK") {
-  //   return res.sendStatus(500);
-  // }
-
-  return res.json("response").sendStatus(201);
+  const response = await createForumUseCase.execute(newForum);
+  return res.send(response).status(201);
 };
 
 const getForumById = async (req: Request, res: Response) => {
@@ -37,8 +41,8 @@ const getForumById = async (req: Request, res: Response) => {
     return res.status(400).send("[error] Invalid forum id");
 
   const response = await getForumByIdUseCase.execute(id);
-  console.log('response: ', response);
-  
+  console.log("response: ", response);
+
   if (!response) return res.sendStatus(404);
   return res.send(response).status(200);
 };
